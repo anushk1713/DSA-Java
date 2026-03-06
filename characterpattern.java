7c43233a42609fc7779b2aacbbb53654a19bbc8a
anushk1713 <kanabaragianush@gmail.com>
2026-03-06 22:54:45 +0530

    Enhance character pattern code with explanatory comment
    Added comment to explain character pattern printing logic.

--- characterpattern.java
+++ characterpattern.java
@@ -5,6 +5,7 @@ public class characterpattern{
         int n = sc.nextInt();
         char c = 'A';
 
+        //charcter pattern printing ex: A = 65 after c++ it will 66=B..loop continues
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
                 System.out.print(c);
