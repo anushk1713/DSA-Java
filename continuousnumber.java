9f1ae1f1a2b6c513c16279aa038f12faa6157c74
anushk1713 <kanabaragianush@gmail.com>
2026-03-06 22:55:58 +0530

    Add explanation comment for number pattern printing
    Added comment to explain continuous number pattern printing.

--- continuousnumber.java
+++ continuousnumber.java
@@ -4,6 +4,8 @@ public class continuousnumber{
         Scanner sc = new Scanner(System.in);
         int num = 1;
         int n = sc.nextInt();
+
+        //continuous number pattern printing ex: num =1 after increment it will be 234567...loop continues
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
                 System.out.print(num);
