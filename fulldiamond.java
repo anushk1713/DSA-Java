40ac25c0e646e5272b6bca40bda5aaaab7f166a1
anushk1713 <kanabaragianush@gmail.com>
2026-03-06 22:58:49 +0530

    Add outer and inner loops for diamond pattern

--- fulldiamond.java
+++ fulldiamond.java
@@ -7,16 +7,22 @@ public class fulldiamond {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
+        //outer loop for row 1st loop
         for (int i = 1; i <= n; i++) {
+            
+            //1st inner loop for space printing
             for (int s = 1; s <= n - i; s++) {
                 System.out.print(" ");
             }
+
+            //2nd inner loop for pattern *
             for (int j = 1; j <= 2 * i - 1; j++) {
                 System.out.print(j);//print("*")
             }
             System.out.println();
         }
 
+        //2nd loop
         for (int i = n - 1; i >= 1; i--) {
             for (int s = 1; s <= n - i; s++) {
                 System.out.print(" ");
