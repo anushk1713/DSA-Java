cba1e0874079350bab09e044bfc1936961a4fb12
anushk1713 <kanabaragianush@gmail.com>
2026-03-06 23:07:25 +0530

    Implement hollow square pattern printing
    Added logic to print a hollow square pattern based on user input.

--- hollow.java
+++ hollow.java
@@ -4,8 +4,11 @@ public class hollow{
      Scanner sc = new Scanner(System.in);
      int n = sc.nextInt();   
 
+    //outer loop for rows
      for(int i=1;i<=n;i++){
+         //inner loop decides what element should be there in a row
         for(int j=1;j<=n;j++){
+            //condition to for hollow print
             if(i==1 || i==n || j==1 || j==n)
               System.out.print("*");
             else 
