342b443b391fc5a14086580e483122a7006d68ed
anushk1713 <kanabaragianush@gmail.com>
2026-03-06 22:57:14 +0530

    Add comments to clarify output of number pattern
    Added comments to explain the output pattern of the nested loops.

--- fixednumpattern.java
+++ fixednumpattern.java
@@ -4,6 +4,8 @@ public class fixednumpattern{
     public static void main(String[] args) {
      Scanner sc = new Scanner(System.in);  
      int n = sc.nextInt();
+
+     // it prints same number as row number if row is 1 then it prints 1111 2222 3333 4444 for others..loop continues
      for(int i=1;i<=4;i++){
         for(int j=1;j<=4;j++){
             // System.out.print(j);//if we print j o/p is 1234 4 times
