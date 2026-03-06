111d3d1cacf35d5d87301431535e3f6677fd9f20
anushk1713 <kanabaragianush@gmail.com>
2026-03-06 23:09:36 +0530

    Implement palindromic pyramid pattern in Java
    Added loops to create a palindromic pyramid pattern.

--- palindromepyramid.java
+++ palindromepyramid.java
@@ -8,15 +8,17 @@ public class palindromepyramid {
         int n = sc.nextInt();
         int num = 1;
 
+        //outer loop for rows
         for (int i = 1; i <= n; i++) {
+            //inner loop for space
             for (int s = 1; s <= n - i; s++) {
                 System.out.print(" ");
-                
             }
+            //2nd inner loop for incresing the number
             for (int k = 1; k <= i; k++) {
                 System.out.print(k);
-               
             }
+            //3rd inner loop for decreasing the number
             for(int j=i-1;j>=1;j--)
                System.out.print(j);
                 // num++;
