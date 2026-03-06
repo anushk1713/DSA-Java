dd303ec7d261cdcb92352eba33ce38f8d4a13c8e
anushk1713 <kanabaragianush@gmail.com>
2026-03-06 22:52:20 +0530

    Add comment for inverted pattern printing
    Added a comment to clarify the purpose of the pattern printing loop.

--- Downtriangle.java
+++ Downtriangle.java
@@ -3,6 +3,7 @@ public class Downtriangle{
     public static void main(String[] args) {
      Scanner sc = new Scanner(System.in);   
      int n = sc.nextInt();
+        //inverted pattrn printing based on number
      for(int i=n;i>=1;i--){
         for(int j=1;j<=i;j++){
             System.out.print(j);
