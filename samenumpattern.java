d7fd661ba302bc23944f23602b54eb4fd7bd7477
anushk1713 <kanabaragianush@gmail.com>
2026-03-07 23:07:23 +0530

    Implement number pattern printing in samenumpattern
    Add nested loops to print number pattern

--- samenumpattern.java
+++ samenumpattern.java
@@ -4,7 +4,11 @@ public class samenumpattern{
     public static void main(String[] args) {
      Scanner sc = new Scanner(System.in);  
      int n = sc.nextInt();
+
+     //outerloop for rows   
      for(int i=1;i<=4;i++){
+         //innerloop for colmns & prints i --> 1 22 333 4444
+         
         for(int j=1;j<=i;j++){
             System.out.print(i);
         }
