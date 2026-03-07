c422f090b64a39886b9621a1ed5c34fcb8f1e3a8
anushk1713 <kanabaragianush@gmail.com>
2026-03-07 23:05:26 +0530

    Add comments to reverse pattern generation code
    Added comments to clarify the purpose of loops in the pattern generation.

--- reverse.java
+++ reverse.java
@@ -4,7 +4,9 @@ public class reverse{
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
+        //outerloop for rows
         for(int i=1;i<=4;i++){
+            //innerloop for columns & prints pattern in reverse order like 1 21 321 4321
             for(int j=i;j>=1;j--){
                 System.out.print(j);
             }
