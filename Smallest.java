45627703c7d2dd0faf51ddfe298dd1aec18282e6
anushk1713 <kanabaragianush@gmail.com>
2026-03-07 22:58:59 +0530

    Add comments for input handling and min calculation
    Added comments for clarity on input and finding the smallest element.

--- Smallest.java
+++ Smallest.java
@@ -6,11 +6,15 @@ public class Smallest{
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
+
+        //accepting values
         for(int i=0;i<n;i++){
             arr[i] = sc.nextInt();
         }
 
         int min = arr[0];
+        //finds smallest element
+        
         for(int i=1;i<n;i++){
             if(arr[i]<min){
                 min = arr[i];
