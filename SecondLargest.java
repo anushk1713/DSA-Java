afac2fdc8dc3d1b58606450c5f15297a1dbd4ead
anushk1713 <kanabaragianush@gmail.com>
2026-03-07 22:57:38 +0530

    Update SecondLargest.java

--- SecondLargest.java
+++ SecondLargest.java
@@ -4,19 +4,24 @@ public class SecondLargest{
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
+
+        //loop to accept the values
         for(int i=0;i<n;i++){
             arr[i] = sc.nextInt();
         }
-
+        //Integer.MIN_VALUE: automatically sets to min value
         int largest = Integer.MIN_VALUE;
         int secondlargest = Integer.MIN_VALUE;
+
+        //finds largest element
         for(int i=0;i<n;i++){
             if(arr[i]>largest){
                 secondlargest = largest;
                 largest = arr[i];
             }
         }
-        
+
+        //finds secnd largest element
         for(int i=0;i<n-1;i++){
             if(arr[i]>secondlargest && secondlargest != largest){
                 secondlargest = arr[i];
