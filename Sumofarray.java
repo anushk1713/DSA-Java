a14dd52e280376fad1cd25f281c559f922a9dfc8
anushk1713 <kanabaragianush@gmail.com>
2026-03-07 23:00:05 +0530

    Add comments for clarity in Sumofarray.java
    Added comments to clarify input handling and sum calculation.

--- Sumofarray.java
+++ Sumofarray.java
@@ -4,11 +4,14 @@ public class Sumofarray{
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
+
+        //accepts values
         for(int i=0;i<n;i++){
             arr[i] = sc.nextInt();
         }
 
         int sum = 0; 
+        //sum of all values
         for(int i=0;i<n;i++){
             sum += arr[i];
         }
