80d941090c042f0544e746334df6f06013615be6
anushk1713 <kanabaragianush@gmail.com>
2026-03-09 22:41:32 +0530

    Fix formatting issues in reverseAnarray.java

--- reverseAnarray.java
+++ reverseAnarray.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class reverseAnarray{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int n =sc.nextInt();
+        int n = sc.nextInt();
         int[] arr = new int[n];
         //accepting an array values
         for(int i=0;i<n;i++){
@@ -15,3 +15,4 @@ public class reverseAnarray{
     }
 
 }
+
