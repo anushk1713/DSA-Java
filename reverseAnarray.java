145d6aa6d8c4fa00bb54c56f5e96145317eb0abc
anushk1713 <kanabaragianush@gmail.com>
2026-03-09 22:10:28 +0530

    Add comments for clarity in reverseAnarray.java
    Added comments to clarify array input and output.

--- reverseAnarray.java
+++ reverseAnarray.java
@@ -4,13 +4,14 @@ public class reverseAnarray{
         Scanner sc = new Scanner(System.in);
         int n =sc.nextInt();
         int[] arr = new int[n];
-
+        //accepting an array values
         for(int i=0;i<n;i++){
             arr[i] = sc.nextInt();
         }
-
+        //printing an array values in reverse
         for(int i=n-1;i>=0;i--){
             System.out.println(arr[i]);
         }
     }
+
 }
