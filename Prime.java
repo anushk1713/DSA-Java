d56d16aed23e31a8d548663003904d58924ecf94
anushk1713 <kanabaragianush@gmail.com>
2026-03-13 22:44:30 +0530

    Enhance Prime.java with comments for clarity
    Added comments to clarify the prime checking logic.

--- Prime.java
+++ Prime.java
@@ -4,21 +4,22 @@ public class Prime{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
+        //checking whether the n is prime or not
         if(n<=1){
             System.out.print("Not Prime");
         }
         boolean isprime = true;
-
+        //loop to check the number is prime or not
         for(int i=2;i<=Math.sqrt(n);i++){
             if(n%i == 0){
                 System.out.print("not prime");
             }
         }
+        //if it is prime printing prime else not prime
         if(isprime == true){
             System.out.print("Prime");
         }
         else
-        System.out.print("Invalid");
+        System.out.print("Not prime");
     }
 }
