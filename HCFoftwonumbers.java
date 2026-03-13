0d093a377738e04bf5e2bf3e4bdc2557b240e13d
anushk1713 <kanabaragianush@gmail.com>
2026-03-13 22:48:15 +0530

    Enhance HCF calculation with comments
    Added comments to clarify input checks and loop functionality.

--- HCFoftwonumbers.java
+++ HCFoftwonumbers.java
@@ -5,17 +5,22 @@ public class HCFoftwonumbers{
         int a = sc.nextInt();
         int b = sc.nextInt();
         int smaller;
+        //checking whether the accpeted input has smaller for next loop
         if(a<b){
             smaller = a;
         }
         else 
             smaller = b;
         int hcf = 0;
+
+        //loop runs from 1 to smaller one
         for(int i=1;i<smaller;i++){
+            //checks whether a & b == 0 or not if both are true i value will be stored in the hcf
             if(a%i==0 && b%i==0){
                 hcf=i;
             }
         }
+        //print hcf
         System.out.print(hcf);
     }
 }
