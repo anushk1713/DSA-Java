c1db3a6582926bfa769fcca0b9715757873b8bae
anushk1713 <kanabaragianush@gmail.com>
2026-03-13 22:45:33 +0530

    Add comments to Primetest class for clarity
    Added comments to clarify the prime number checking logic.

--- Primetest.java
+++ Primetest.java
@@ -5,16 +5,18 @@ public class Primetest{
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         boolean isprime = true;
-
+        //condition to check the number is prime or not
         if(n<=1){
             isprime = false;
         }else{
+            //loop for checking the prime
             for(int i=2;i<=n/2;i++){
                 if(n%2==0){
                     isprime = false;
                     break;
                 }
             }
+            //if prime print prime else not prime
             if(isprime){
                 System.out.print("Prime");
             }
