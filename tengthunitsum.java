d92892414166ceb25bb204b85673ee26de4cb4d5
anushk1713 <kanabaragianush@gmail.com>
2026-03-13 22:50:43 +0530

    Enhance tengthunitsum.java with comments
    Added comments to clarify input and logic for sum calculation.

--- tengthunitsum.java
+++ tengthunitsum.java
@@ -2,8 +2,11 @@ import java.util.Scanner;
 public class tengthunitsum{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+
+        //accept the n
         int n = sc.nextInt();
-        int sum = 0;
+        int sum = 0; // initialize the sum = 0
+        //math logic to print the tength ad unit digit sum
         sum = n%10+(n/10)%10;
         System.out.print(sum);
     }
