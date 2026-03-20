5e2911ab6d07a14a622cbdbe675a62c9caa242a6
anushk1713 <kanabaragianush@gmail.com>
2026-03-20 21:34:01 +0530

    Add comments for better code clarity in SortingAnArray
    Added comments to clarify code functionality.

--- SortingAnArray.java
+++ SortingAnArray.java
@@ -2,19 +2,23 @@ import java.util.Scanner;
 public class SortingAnArray{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        //n for array size
         int n = sc.nextInt();
+        //for storing the values of same data type
         int[] arr = new int[n];
+        //assuming array is sorted
         boolean isSorted = true;
-
+        //values
         for(int i=0;i<n;i++){
             arr[i] = sc.nextInt();
         }
-
+        //checking whether the array is sorted or not
         for(int i=0;i<n-1;i++){
             if(arr[i]>arr[i+1]){
                 isSorted = false;
             }
         }
+        //if array is sorted print sorted else not sorted
             if(isSorted == true){
                 System.out.print("Array is sorted");
             }
