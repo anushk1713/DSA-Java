dcd2c1bf4c5ced088524b8b17117c5d5f9ad9850
anushk1713 <kanabaragianush@gmail.com>
2026-03-20 20:30:42 +0530

    add source code of ReversingInsideSameArray code

---
+++ ReversingInsideSameArray.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class ReversingInsideSameArray{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i=0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+        for(int num = 0; num<arr.length; num++){
+            System.out.print(arr[num] + " ");
+        }
+    }
+}
