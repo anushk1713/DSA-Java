1d07c8deec3d22b6bdd319beb1ff36433d2225ba
anushk1713 <kanabaragianush@gmail.com>
2026-03-23 21:34:52 +0530

    Enhance CharacterRemove.java with comments
    Added comments to improve code readability.

--- CharacterRemove.java
+++ CharacterRemove.java
@@ -2,19 +2,24 @@ import java.util.Scanner;
 public class CharacterRemove{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        //accepts String input
         String str = sc.next();
+        //calulates string length
         int n = str.length();
+        //to store the string as array
         int[] arr = new int[n];
         for(int i=0;i<n;i++){
+            //string to char
             arr[i] = str.charAt(i);
         }
-
+        //loop to remove characters first and last of string
         for(int i=1;i<n-1;i++){
             if(i==0 && i == n-1){
                 continue;
             }
 
         }
+        //print string
         System.out.print(str);
 
     }
