a31758b9f025cb6cf70c4152eacec3fe8ee1cbc2
anushk1713 <kanabaragianush@gmail.com>
2026-03-23 23:26:35 +0530

    Enhance Fibonacci.java with explanatory comments
    Added comments to explain the Fibonacci sequence logic and variable usage.

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,19 +1,24 @@
-
+//In this program we simply need addition of two numbers previous and next element but it prints only the sum. but in Fibonacci numbers starts from 0 & 1
+//so we will initialize the 0 & 1 and then print it first then print sum of elements.
 import java.util.Scanner;
 
 
 public class Fibonacci{
     public static void main(String[] args){
         Scanner sc =new Scanner(System.in);
-        int n = sc.nextInt();
-        long prev1 = 0;
-        long prev2 = 1;
-        System.out.print(prev1 + " " + prev2 + " ");
+        int n = sc.nextInt();//accpets n value
+        long prev1 = 0;//previous
+        long prev2 = 1;//previous
+        System.out.print(prev1 + " " + prev2 + " ");//prints previous1 & 2
+        //for loop for calculating sum
         for(int i=0;i<n-2;i++){
+            //long datatype is used bcz if we want large fibonacci of large number we need lot of storage so int capacity not suitable therefore we use long
+            //sum of previous and next element
             long next = prev1 + prev2;
+            //print next
             System.out.print(next + " ");
-            prev1 = prev2;
-            prev2 = next;
+            prev1 = prev2;//updates prev1 to prev2
+            prev2 = next;//updates prev2 to next
         }
     }
 }
