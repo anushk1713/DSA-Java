a3c2c17523b9542902214036f36b08e977001150
anushk1713 <kanabaragianush@gmail.com>
2026-03-24 19:20:22 +0530

    Enhance SumofInt.java with comments
    Added comments to clarify code functionality.

--- SumofInt.java
+++ SumofInt.java
@@ -5,18 +5,19 @@ public class SumofInt{
     public static void main(String[] args){
        
         Scanner sc = new Scanner(System.in);
+        //accept the string
         String str = sc.nextLine();
-
+        //split the string to get string as array
         String[] arr = str.split(" ");
-
+        //sum = 0
         int sum = 0;
-
+        //loop for converting array to int
         for(int i=0;i<arr.length;i++){
-            int num = Integer.parseInt(arr[i]);
-            sum = sum+num;
+            int num = Integer.parseInt(arr[i]);//converts array to int
+            sum = sum+num;//sum ups the values
 
         }
-
+        //prints sum
         System.out.print(sum);
         
     }
