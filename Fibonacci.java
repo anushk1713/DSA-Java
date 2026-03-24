92b11240e295aa4f3cc084c6f5adef03908d17cd
anushk1713 <kanabaragianush@gmail.com>
2026-03-24 19:16:21 +0530

    Enhance Fibonacci.java with comments
    Added comments to improve code readability and understanding.

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,14 +1,16 @@
-
+//for Fibonacci we need two variables one is previous1 and previous2 bcz it start from 0,1. and we use long datatypw for it
 import java.util.Scanner;
-
-
 public class Fibonacci{
     public static void main(String[] args){
         Scanner sc =new Scanner(System.in);
+        //accept the n value
         int n = sc.nextInt();
+        //initialize prev1 & prev2
         long prev1 = 0;
         long prev2 = 1;
+        //print it
         System.out.print(prev1 + " " + prev2 + " ");
+        //loop to achieve the Fibonacci
         for(int i=0;i<n-2;i++){
             long next = prev1 + prev2;
             System.out.print(next + " ");
