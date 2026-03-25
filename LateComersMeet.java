da2acf9c4c68fe8d9ae30480a745dd94b3f61414
anushk1713 <kanabaragianush@gmail.com>
2026-03-25 17:35:54 +0530

    Enhance LateComersMeet.java with comments
    Added comments to explain the code logic.

--- LateComersMeet.java
+++ LateComersMeet.java
@@ -2,20 +2,22 @@ import java.util.Scanner;
 public class LateComersMeet{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        //accept the string
         String str = sc.nextLine();
-
+        //Split the String by space
         String[] arr = str.split(" ");
-        int count = 0;
+        int count = 0;//set count = 0
         
-        
+        //for loop to split the string & to convert the String to Integer
         for(int i=0;i<arr.length;i++){
             String[] hours = arr[i].split(":");
-            int hr = Integer.parseInt(hours[0]);
-            int minute = Integer.parseInt(hours[1]);
+            int hr = Integer.parseInt(hours[0]);//Hours
+            int minute = Integer.parseInt(hours[1]);//Minutes
+            //condition whether the student came to college within 10, if not count them
             if(hr > 10 || (hr==10 && minute > 0)){
-                count++;
+                count++;//counting the late comers
             }
         }
-        System.out.println(count);
+        System.out.println(count);//printing number of late comers
     }
 }
